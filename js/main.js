f749bb74a4206c135f47a6218aabe654b9bc9057
Yehor Chasnovskyi <egor.chasnovskii@gmail.com>
2021-01-27 18:45:19 +0200

    Add project section.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
-  new Splide(".splide", {
+  new Splide(".news__slider", {
     type: "loop",
     perPage: 1,
     autoWidth: true,
     focus: "center",
     gap: "29rem",
+    pagination: false,
+  }).mount();
+
+  new Splide(".projects__slider", {
+    type: "loop",
+    perPage: 1,
+    autoWidth: true,
+    focus: "center",
+    gap: "20rem",
     pagination: false,
   }).mount();
 });
